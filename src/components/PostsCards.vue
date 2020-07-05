b0b04a13975adf255d82597118242e7855c6564f
jh <1139209675@qq.com>
2020-07-05 15:44:40 +0800

    api

--- src/components/PostsCards.vue
+++ src/components/PostsCards.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col cols="12">
+    <!-- <v-col cols="12">
       <v-card color="#385F73" dark min-height="200">
         <v-card-title class="headline">Unlimited music now</v-card-title>
 
@@ -10,21 +10,24 @@
           <v-btn text>Listen Now</v-btn>
         </v-card-actions>
       </v-card>
-    </v-col>
+    </v-col> -->
 
     <v-col v-for="(item, i) in items" :key="i" cols="12">
-      <v-card :color="item.color" dark min-height="200">
+      <v-card color="#1F7087" dark min-height="200">
         <div class="d-flex flex-no-wrap justify-space-between">
           <div>
-            <v-card-title class="headline" v-text="item.title"></v-card-title>
+            <v-card-title class="headline" v-text="item.article_title"></v-card-title>
 
-            <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            <v-card-subtitle v-text="item.sub_title"></v-card-subtitle>
           </div>
 
           <v-avatar class="ma-3" size="125" tile>
-            <v-img :src="item.src"></v-img>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
           </v-avatar>
         </div>
+        <v-card-actions>
+          <v-btn text :href="`http://blog.jackdou.top/${item.id}/show`">查 看</v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
     <v-col cols="12">
@@ -64,10 +67,16 @@ export default {
     getPosts: function(page = null) {
       let p = page === null ? this.page : page;
       console.log(this.url);
+      let vm = this;
       axios
         .get(this.url + `?page=${p}&limit=${this.limit}`)
         .then(res => {
-          console.log(res);
+          console.log(res.data);
+          let list = res.data.list;
+          let count = parseInt(res.data.count);
+          vm.length = Math.ceil(count / vm.limit);
+          vm.items = list;
+          
         })
         .catch(res => {
           console.log(res);
